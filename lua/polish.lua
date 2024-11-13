b429139e2f5e29b8ecbe8974c9cbb0f8ca19cc3a
Fabian Barajas <fbarajas@nvidia.com>
2024-11-13 10:21:40 -0800

    small adjustment to if statements

--- lua/polish.lua
+++ lua/polish.lua
@@ -34,10 +34,14 @@ local function yaml_ft(path, bufnr)
 
   -- check if file is in roles, tasks, or handlers folder
   local path_regex = vim.regex "(tasks\\|roles\\|handlers)/"
-  if path_regex and path_regex:match_str(path) then return "yaml.ansible" end
+  if path_regex and path_regex:match_str(path) then
+    return "yaml.ansible"
+  end
   -- check for known ansible playbook text and if found, return yaml.ansible
   local regex = vim.regex "hosts:\\|tasks:"
-  if regex and regex:match_str(content) then return "yaml.ansible" end
+  if regex and regex:match_str(content) then
+    return "yaml.ansible"
+  end
 
   -- return yaml if nothing else
   return "yaml"
