af9aa044e71f953378814b33391076cf6b88b042
Fabian Barajas <fbarajas@nvidia.com>
2025-03-08 19:32:24 -0800

    cleanup comments

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -38,21 +38,19 @@ return {
       return opts
     end,
   },
-
   -- You can disable default plugins as follows:
-  { "max397574/better-escape.nvim", enabled = false },
-
-  -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
+  {
+    "max397574/better-escape.nvim",
+    enabled = false,
+  },
   {
     "L3MON4D3/LuaSnip",
     config = function(plugin, opts)
       require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom luasnip configuration such as filetype extend or custom snippets
       local luasnip = require "luasnip"
       luasnip.filetype_extend("javascript", { "javascriptreact" })
     end,
   },
-
   {
     "windwp/nvim-autopairs",
     config = function(plugin, opts)
@@ -103,14 +101,7 @@ return {
       require("gitlab").setup()
     end,
   },
-  -- {
-  --   "github/copilot.vim",
-  --   lazy = false,
-    -- event = "InsertEnter",
-    -- config = function()
-    -- end
-  -- },
-  { -- further customize the options set by the community
+  {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
     event = "InsertEnter",
@@ -141,11 +132,7 @@ return {
     lazy = false,
     requires = "nvim-lua/plenary.nvim",
     config = function()
-      require("todo-comments").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
+      require("todo-comments").setup({})
     end
   },
   {
@@ -153,9 +140,7 @@ return {
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     config = function()
-      require("nvim-surround").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
+      require("nvim-surround").setup({})
     end
   },
   {
