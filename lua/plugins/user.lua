dda22b43b87177b0d30aaed7f65d8bdb5ba52b56
Fabian Barajas <fbarajas@nvidia.com>
2025-12-26 02:36:21 -0800

    feat(jira): add jira.nvim plugin
    Integrates Jira functionality into Neovim.
    Configures Jira API access using environment variables.

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -124,6 +124,22 @@ return {
     config = function() require("gitlab").setup() end,
   },
   {
+    "letieu/jira.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    opts = {
+      jira = {
+        base = os.getenv("JIRA_BASE_URL") or "https://your-domain.atlassian.net",
+        email = os.getenv("JIRA_EMAIL") or "your-email@example.com",
+        token = os.getenv("JIRA_API_TOKEN") or "your-api-token",
+        limit = 500,
+      },
+    },
+  },
+  {
     "stevearc/dressing.nvim",
     opts = {
       select = {
