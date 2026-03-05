05c75086b7d81e8395b8baef588043988fd3ad6b
Fabian Barajas <fbarajas@nvidia.com>
2026-03-05 01:53:29 -0800

    feat(plugins): add cursor-agent.nvim plugin

---
+++ lua/plugins/cursor-agent-nvim.lua
@@ -0,0 +1,13 @@
+return {
+  --- @module 'cursor-agent'
+  {
+    "Sarctiann/cursor-agent.nvim",
+    dependencies = {
+      "folke/snacks.nvim",
+    },
+    --- @type Cursor-Agent.Config
+    opts = {
+      -- Configure your options here
+    },
+  }
+}
