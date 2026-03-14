b0d63c7b126e1f96a6706392aa2fc449498814e4
Fabian Barajas <fbarajas@nvidia.com>
2026-03-14 13:54:04 -0700

    feat(plugins): add codock.nvim configuration

---
+++ lua/plugins/codock-nvim.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    'gitsang/codock.nvim',
+    opts = {
+      width = 80, -- Width of the vertical split
+      fixed_width = false, -- Whether to fix the width (true = locked, false = adjustable)
+      codock_cmd = "crush", -- Command to run in the terminal (crush, opencode, claude, gemini-cli, etc.)
+      copy_to_clipboard = false, -- Copy to system clipboard
+      actions = {},
+    },
+    cmd = { "Codock", "CodockFilePos", "CodockActions" },
+    keys = {
+      { "<leader>tCt", "<cmd>Codock<cr>", desc = "Toggle Codock", mode = { "n", "v" } },
+      { "<leader>tCp", ":'<,'>CodockFilePos<cr>", desc = "Copy file path and line info", mode = { "n", "v" } },
+      { "<leader>tCa", ":'<,'>CodockActions<cr>", desc = "Run Codock actions", mode = { "n", "v" } },
+    },
+  }
+}
