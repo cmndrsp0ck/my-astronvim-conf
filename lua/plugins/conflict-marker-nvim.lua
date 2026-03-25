87b2585643cb017144e4f5aa134f1b5ee35ff465
Fabian Barajas <fbarajas@nvidia.com>
2026-03-25 16:25:36 -0700

    feat(plugins): replace conflict-marker.vim with conflict-marker.nvim
    Adds the Neovim version of the conflict-marker plugin and disables the
    older Vim-based version.

---
+++ lua/plugins/conflict-marker-nvim.lua
@@ -0,0 +1,8 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+return {
+  {
+    'cmndrsp0ck/conflict-marker.nvim',
+    url = 'https://gitlab.com/cmndrsp0ck/conflict-marker.nvim',
+  },
+}
